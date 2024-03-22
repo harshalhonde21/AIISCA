2ddf9e6891bcbc13a23994a769d7e470c37f972c
harshalhonde21 <harshalhonde17@gmail.com>
2024-03-22 23:40:50 +0530

    updated

--- frontend/src/Components/NoCounting.jsx
+++ frontend/src/Components/NoCounting.jsx
@@ -3,8 +3,7 @@ import { HiMiniUsers } from "react-icons/hi2";
 import { MdOutlineInsertPhoto } from "react-icons/md";
 import { BsCalendar4Event } from "react-icons/bs";
 import { LiaAwardSolid } from "react-icons/lia";
-
-
+import CountUp from 'react-countup';
 import "./EventContainer.css";
 
 const NoCounting = () => {
@@ -14,26 +13,34 @@ const NoCounting = () => {
                 <div className="count-sub-container">
                     <div className="react-icons"><HiMiniUsers /></div>
                     <div className="name-count">Members</div>
-                    <div className="count-number">200</div>
+                    <div className="count-number">
+                        <CountUp start={0} end={200} duration={2.5} separator="," />
+                    </div>
                 </div>
                 <div className="count-sub-container">
                     <div className="react-icons"><MdOutlineInsertPhoto /></div>
                     <div className="name-count">Photo</div>
-                    <div className="count-number">200</div>
+                    <div className="count-number">
+                        <CountUp start={0} end={200} duration={2.5} separator="," />
+                    </div>
                 </div>
                 <div className="count-sub-container">
                     <div className="react-icons"><BsCalendar4Event /></div>
                     <div className="name-count">Event</div>
-                    <div className="count-number">11+</div>
+                    <div className="count-number">
+                        <CountUp start={0} end={11} duration={2.5} />+
+                    </div>
                 </div>
                 <div className="count-sub-container">
                     <div className="react-icons"><LiaAwardSolid /></div>
                     <div className="name-count">Awards</div>
-                    <div className="count-number">1+</div>
+                    <div className="count-number">
+                        <CountUp start={0} end={1} duration={2.5} />+
+                    </div>
                 </div>
             </div>
         </Fragment>
     )
 }
 
-export default NoCounting
+export default NoCounting;
