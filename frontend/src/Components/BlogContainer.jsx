588c63eef495e6b676a01ffdd9f65055e9f823d5
harshalhonde21 <harshalhonde17@gmail.com>
2024-03-22 21:22:31 +0530

    done

---
+++ frontend/src/Components/BlogContainer.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import "./BlogContainer.css";
+
+const BlogContainer = () => {
+    return (
+        <Fragment>
+            <div className="blog-outer-container">
+                
+            </div>
+        </Fragment>
+    )
+}
+
+export default BlogContainer
--- frontend/src/Components/LeaderContainer.jsx
+++ frontend/src/Components/LeaderContainer.jsx
@@ -14,6 +14,7 @@ const LeaderContainer = () => {
             <div className="name-leader">Harshal</div>
           </div>
         </div>
+        <button className="leader-button">Read More</button>
       </div>
     </Fragment>
   )
