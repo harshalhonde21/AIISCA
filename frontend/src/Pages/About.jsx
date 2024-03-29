c3303c5590ffbfa3c6fc35830eee3f708f87074e
harshalhonde21 <harshalhonde17@gmail.com>
2024-03-30 00:29:28 +0530

    updated

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -6,8 +6,10 @@ import Footer from "../Components/Footer";
 const About = () => {
   return (
     <Fragment>
-      <div className="outer-about-main-component">
-        <div className="front-page-component">
+      <main className="outer-about-main-component">
+        {/* main first component start here */}
+
+        <section className="front-page-component">
           <div className="para-with-name-container">
             <p>
               <span>&ldquo;</span>
@@ -21,19 +23,105 @@ const About = () => {
           <div className="image-about-component">
             <img src="/baba.png" alt="loading.." />
           </div>
-        </div>
-        <div className="seconds-page-container">
+        </section>
+
+        {/* main first component end here */}
+
+        {/* second page start here */}
+
+        <section className="seconds-page-container">
           <AboutContainer />
-        </div>
+        </section>
+
+        {/* second page end here */}
 
         {/* third page start here */}
-        <div className="third-page-container"></div>
+
+        <section className="third-page-container">
+          <h1 className="third-focus-component">KEY AREAS OF FOCUS</h1>
+          <div className="head-para-combine-component">
+            <h4 className="heading-combine">Grassroots Mobilization</h4>
+            <p className="para-combine">
+              We firmly believe that change begins at the grassroots level. As
+              such, we actively engage in grassroots mobilization efforts,
+              collaborating with Dalit communities to identify their unique
+              needs and challenges. Through community-led initiatives and
+              projects, we empower Dalit individuals with the tools and
+              knowledge to advocate for their rights and create a positive
+              impact within their communities.
+            </p>
+
+            <h4 className="heading-combine">Advocacy and Awareness</h4>
+            <p className="para-combine">
+              One of our primary objectives is to raise awareness about the
+              systemic challenges faced by Dalits due to caste-based
+              discrimination. Through educational campaigns, workshops,
+              seminars, and social media outreach, we aim to sensitize the
+              public to the enduring impact of casteism. By shedding light on
+              these issues, we work towards transforming societal perceptions
+              and breaking down discriminatory practices.
+            </p>
+
+            <h4 className="heading-combine">Political Engagement</h4>
+            <p className="para-combine">
+              To effect lasting change, we recognize the significance of
+              political engagement. Our movement advocates for Dalit
+              representation in political spheres and works to strengthen the
+              political voice of Dalits at local, regional, and national levels.
+              We support and endorse leaders who champion Dalit rights and work
+              towards inclusive policies and legislation.
+            </p>
+
+            <h4 className="heading-combine">Solidarity and Collaboration</h4>
+            <p className="para-combine">
+              Unity is the cornerstone of our movement. We actively seek
+              alliances with other social justice movements, organizations, and
+              individuals that share our vision of equality and justice. By
+              fostering collaboration and mutual support, we amplify our
+              collective efforts to dismantle caste-based oppression and promote
+              a society that values the dignity and humanity of every
+              individual.
+            </p>
+
+            <h4 className="heading-combine">
+              Cultural Revival and Empowerment
+            </h4>
+            <p className="para-combine">
+              We celebrate Dalit culture and heritage, recognizing the richness
+              and diversity it brings to the world. Our movement endeavors to
+              promote cultural revival, preserving traditional knowledge, arts,
+              and customs. Through cultural events and initiatives, we aim to
+              instill pride and self-esteem within Dalit communities and
+              challenge stigmatizing stereotypes.
+            </p>
+          </div>
+        </section>
+
         {/* third page End here */}
+
+        {/* fourth and last page start here */}
+
+        <section className="fourth-page-container">
+          <h1 className="fourth-focus-component">OUR OBJECTIVES</h1>
+          <div className="list-of-work">
+            <ul>
+              <li>To unite all the erstwhile untouchable (Dalit) communities in one Umbrella organisation to fight against the hegemonic exploitative and brahmanical forces.</li>
+              <li>To build autonomous social, economical, educational and cultural institutions.</li>
+              <li>To promote the idea of Liberty, Equality and Fraternity among all the Dalit communities.</li>
+              <li>To cultivate and nourish the culture of Educate, Agitate and Organize among the Dalit communities for social mobility and transformation.</li>
+              <li>To provide local as well as national well organized Ambedkarite leadership to the Dalit community.</li>
+              <li>To fight for the constitutional Rights and Entitlements, Welfare and Interests of all Dalit communities.</li>
+            </ul>
+          </div>
+        </section>
+
+        {/* fourth and last page start here */}
+
 
         <div className="footer-page-container">
           <Footer />
         </div>
-      </div>
+      </main>
     </Fragment>
   );
 };
