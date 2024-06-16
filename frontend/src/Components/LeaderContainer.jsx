01c3fa25efcc8c24c528bb20a9c8d7b335899a3c
harshalhonde21 <harshalhonde17@gmail.com>
2024-06-16 22:41:02 +0530

    updated the leadership

--- frontend/src/Components/LeaderContainer.jsx
+++ frontend/src/Components/LeaderContainer.jsx
@@ -1,28 +1,40 @@
-import {Fragment} from 'react';
+import { Fragment } from "react";
 import "./LeaderContainer.css";
 
 const LeaderContainer = () => {
   return (
     <Fragment>
       <div className="leader-component">
-        <h1 className="leader-heading-component">
-          OUR LEADERSHIP TEAM
-        </h1>
+        <h1 className="leader-heading-component">OUR LEADERSHIP TEAM</h1>
         <div className="main-leader-component">
           <div className="card-team-member">
-            <img src="/logo.png" alt="" />
-            <div className="name-leader">Harshal</div>
+            <img src="/leader1.png" alt="" />
+            <div className="name-leader">Dr. Rahul Sonpimple</div>
           </div>
           <div className="card-team-member">
-            <img src="/logo.png" alt="" />
-            <div className="name-leader">Harshal</div>
+            <img src="/leader2.png" alt="" />
+            <div className="name-leader">Vishal Sarpe</div>
           </div>
-          
+          <div className="card-team-member">
+            <img src="/leader3.png" alt="" />
+            <div className="name-leader">Radhika Vemula</div>
+          </div>
+          <div className="card-team-member">
+            <img src="/leader4.png" alt="" />
+            <div className="name-leader">Amarjit Gurto</div>
+          </div>
+          <div className="card-team-member">
+            <img src="/leader5.png" alt="" />
+            <div className="name-leader">Abhijit Waghre</div>
+          </div>
+          <div className="card-team-member">
+            <img src="/leader6.png" alt="" />
+            <div className="name-leader">Vikas Kumar Moola</div>
+          </div>
         </div>
-        {/* <button className="leader-button">Read More</button> */}
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default LeaderContainer
+export default LeaderContainer;
