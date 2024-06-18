35cde090d79f52bf7a94e6956a0b71eaf09311e5
Harshal Honde <105191619+harshalhonde21@users.noreply.github.com>
2024-06-18 22:06:33 +0530

    updated

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <Fragment>
       <video autoPlay muted loop>
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="/videoav.mov" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="event-container">
